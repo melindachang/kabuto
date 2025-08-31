0fb4a5660d4ae48f6ecd5a01dd1c43c71f44e080
Melinda Chang <melindachang.hy@gmail.com>
2025-08-30 23:43:36 -0400

    feat(sidebar): structure for first layer

--- web/src/lib/components/app-sidebar.svelte
+++ web/src/lib/components/app-sidebar.svelte
@@ -1,3 +1,111 @@
 <script lang="ts">
   import * as Sidebar from '$lib/components/ui/sidebar';
+
+  import { House, BookOpen, Film, Gamepad2, Command, Tv } from '@lucide/svelte';
+  import type { ComponentProps } from 'svelte';
+  const data = {
+    nav_main: [
+      {
+        title: 'Home',
+        url: '/',
+        icon: House,
+        isActive: true
+      },
+      {
+        title: 'Books',
+        url: '/books',
+        icon: BookOpen,
+        isActive: false
+      },
+      {
+        title: 'Films',
+        url: '/films',
+        icon: Film,
+        isActive: false
+      },
+      {
+        title: 'Shows',
+        url: '/shows',
+        icon: Tv,
+        isActive: false
+      },
+      {
+        title: 'Games',
+        url: '/games',
+        icon: Gamepad2,
+        isActive: false
+      }
+    ]
+  };
+
+  let {
+    ref = $bindable(null),
+    ...restProps
+  }: ComponentProps<typeof Sidebar.Root> = $props();
+
+  let activeItem = $state(data.nav_main[0]);
+  const sidebar = Sidebar.useSidebar();
 </script>
+
+<Sidebar.Root
+  bind:ref
+  collapsible="icon"
+  class="overflow-hidden [&>[data-sidebar=sidebar]]:flex-row"
+  {...restProps}
+>
+  <Sidebar.Root
+    collapsible="none"
+    class="!w-[calc(var(--sidebar-width-icon)_+_1px)] border-r"
+  >
+    <Sidebar.Header>
+      <Sidebar.Menu
+        ><Sidebar.MenuItem>
+          <Sidebar.MenuButton size="lg" class="md:h-8 md:p-0">
+            {#snippet child({ props })}
+              <a href="##" {...props}>
+                <div
+                  class="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground"
+                >
+                  <Command class="size-4" />
+                </div>
+              </a>
+            {/snippet}
+          </Sidebar.MenuButton>
+        </Sidebar.MenuItem></Sidebar.Menu
+      >
+    </Sidebar.Header>
+    <Sidebar.Content>
+      <Sidebar.Group>
+        <Sidebar.GroupContent class="px-15 md:px-0">
+          <Sidebar.Menu>
+            {#each data.nav_main as item (item.title)}
+              <Sidebar.MenuItem>
+                <Sidebar.MenuButton
+                  tooltipContentProps={{
+                    hidden: false
+                  }}
+                  onclick={() => {
+                    activeItem = item;
+                    sidebar.setOpen(true);
+                  }}
+                  isActive={activeItem.title === item.title}
+                  class="px-2.5 md:px-2"
+                >
+                  {#snippet tooltipContent()}
+                    {item.title}
+                  {/snippet}
+                  <item.icon />
+                  <span>{item.title}</span>
+                </Sidebar.MenuButton>
+              </Sidebar.MenuItem>
+            {/each}
+          </Sidebar.Menu>
+        </Sidebar.GroupContent>
+      </Sidebar.Group>
+    </Sidebar.Content>
+    <Sidebar.Footer></Sidebar.Footer>
+  </Sidebar.Root>
+  <Sidebar.Root collapsible="none" class="hidden flex-1 md:flex">
+    <Sidebar.Header></Sidebar.Header>
+  </Sidebar.Root>
+</Sidebar.Root>
--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,12 +1,43 @@
 <script lang="ts">
-	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
+  import '../app.css';
+  import favicon from '$lib/assets/favicon.svg';
 
-	let { children } = $props();
+  import * as Sidebar from '$lib/components/ui/sidebar';
+  import * as Breadcrumb from '$lib/components/ui/breadcrumb';
+  import Separator from '$lib/components/ui/separator/separator.svelte';
+  import AppSidebar from '$lib/components/app-sidebar.svelte';
+
+  let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+  <link rel="icon" href={favicon} />
 </svelte:head>
+
+<Sidebar.Provider>
+  <AppSidebar />
+  <Sidebar.Inset>
+    <header
+      class="sticky top-0 flex shrink-0 items-center gap-2 border-b bg-background p-4"
+    >
+      <Sidebar.Trigger class="-ml-1" />
+      <Separator
+        orientation="vertical"
+        class="mr-2 data-[orientation=vertical]:h-4"
+      />
+      <Breadcrumb.Root>
+        <Breadcrumb.List>
+          <Breadcrumb.Item class="hidden md:block">
+            <Breadcrumb.Link href="#">Root</Breadcrumb.Link>
+          </Breadcrumb.Item>
+          <Breadcrumb.Separator class="hidden md:block" />
+          <Breadcrumb.Item>
+            <Breadcrumb.Link href="#">Second</Breadcrumb.Link>
+          </Breadcrumb.Item>
+        </Breadcrumb.List>
+      </Breadcrumb.Root>
+    </header>
+  </Sidebar.Inset>
+</Sidebar.Provider>
 
 {@render children?.()}
